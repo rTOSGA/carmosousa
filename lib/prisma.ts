0fab2634d1c38509c467f3f642bfa8aa35a291e0
Ricardo Sousa <ricardotosga@gmail.com>
2022-12-30 22:39:30 +0000

    fix-prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,10 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function main() {
-  
-}
+async function main() {}
+
+export default prisma;
 
 main()
   .catch((e) => {
