e9f9fe0a8ad64bd45330b28fb7016b077db36f5b
Ricardo Sousa <ricardotosga@gmail.com>
2022-12-30 22:31:00 +0000

    fix-tags

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,14 @@
-import Banner from "../components/banner";
-import Body from "../components/body";
 import Carousel from "../components/carousel";
-
 import Tags from "../components/tags";
+
+
 
 export default function Home() {
   return (
     <div className="flex flex-col w-full">
-      
-      
       <Tags></Tags>
       {/* <hr className="flex my-3 border-gray-300 drop-shadow-2xl w-screen" /> */}
-      <Carousel></Carousel> 
+      <Carousel></Carousel>
 
       <Carousel></Carousel>
     </div>
--- components/tags.tsx
+++ components/tags.tsx
@@ -1,23 +1,23 @@
-import { link } from "fs";
-import prisma from "../lib/prisma";
+const data = [
+  { name: "Ramos" },
+  { name: "Plantas" },
+  { name: "Rosas Vermelhas" },
+  { name: "Tulipas Amarelas" },
+  { name: "Decoração" },
+  { name: "Jarra" },
+  { name: "Novidades" },
+];
 
-async function getTags() {
-  const tags = await prisma.tag.findMany();
-  return tags;
-}
-
-export default async function Tags() {
-  const data = await getTags();
-  console.log(data);
+export default function Tags() {
   return (
     <div className="flex flex-wrap w-full justify-center p-7 pb-0 text-sm  text-white ">
       {data.map((i) => (
-        <span
+        <li
           className="bg-emerald-900 p-1 px-3 mr-3 mb-1 rounded-full hover:bg-emerald-700 transition-all duration-300 cursor-pointer"
           key={i.name}
         >
           {i.name}
-        </span>
+        </li>
       ))}
     </div>
   );
